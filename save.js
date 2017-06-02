274f283785a47515d75eec66ef948e24b6edff90
Vuk Petrovic <vukpetrovic1@yahoo.com>
2017-06-02 19:28:55 -0400

    Updated save.js

--- save.js
+++ save.js
@@ -3,7 +3,7 @@ var fs = require('fs');
 var profile_path = require('os').homedir() + "/.bash_profile";
 
 try {
-	fs.appendFileSync(profile_path, "\nalias webqs='git clone https://github.com/Grandclosing/WebQuickStart.git . && ls'");
+	fs.appendFileSync(profile_path, "\nalias webqs='git clone https://github.com/Grandclosing/WebQuickStart.git . && rm -rf .git && rm save.js && git init && ls -a'");
 	console.log("WebQuickStart has set up the 'webqs' alias for you. Next time you want to create a quick web project, make a directory, cd into it and run 'webqs'");	
 } catch(e) {
 	console.log(e);
